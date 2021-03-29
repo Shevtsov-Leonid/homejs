2c232708f67775a8b025995efbc2a0c7b1863c1d
Shevtsov-Leonid <weva458@gmail.com>
2021-03-29 13:20:11 +0300

    first js homework

---
+++ first.js
@@ -0,0 +1,377 @@
+//1.створ.змінні consol doc. alert
+
+/*let first = 'hello';
+let abbr = 'owu';
+let com = 'com';
+let ukr = 'ua';
+let number1 = 1;
+let number10 = 10;
+let minus = -999;
+let abc = 123;
+let pi = 3.14;
+let cdi = 2.7;
+let adult = 16;
+let buleT = true;
+let buleF = false;
+
+
+console.log (first);
+console.log ( abbr);
+console.log(com);
+console.log(ukr);
+console.log(number1);
+console.log(number10);
+console.log(minus);
+console.log(abc);
+console.log(pi);
+console.log(cdi);
+console.log(adult);
+console.log(buleT);
+console.log(buleF);
+
+document.write(first);
+document.write(abbr);
+document.write(com);
+document.write(ukr);
+document.write(number1);
+document.write(number10);
+document.write(minus);
+document.write(abc);
+document.write(pi);
+document.write(cdi);
+document.write(adult);
+document.write(buleT);
+document.write(buleF);
+
+
+
+alert(first);
+alert(abbr);
+alert(com);
+alert(ukr);
+alert(number1);
+alert(number10);
+alert(minus);
+alert(abc);
+alert(pi);
+alert(cdi);
+alert(adult);
+alert(buleT);
+alert(buleF);
+
+
+//2.Rename
+ first = 'byeBye';
+ abbr = 'okten';
+ com = 'point';
+ ukr = 'eng';
+ number1 = 11;
+ number10 = 100;
+ minus = -888;
+ abc = 456;
+ pi = 3.148675757;
+ cdi = 2.7;
+ adult = 16;
+ buleT = true;
+ buleF = false;
+
+
+
+
+
+console.log (first);
+console.log ( abbr);
+console.log(com);
+console.log(ukr);
+console.log(number1);
+console.log(number10);
+console.log(minus);
+console.log(abc);
+console.log(pi);
+console.log(cdi);
+console.log(adult);
+console.log(buleT);
+console.log(buleF);
+
+document.write(first);
+document.write(abbr);
+document.write(com);
+document.write(ukr);
+document.write(number1);
+document.write(number10);
+document.write(minus);
+document.write(abc);
+document.write(pi);
+document.write(cdi);
+document.write(adult);
+document.write(buleT);
+document.write(buleF);
+
+
+
+alert(first);
+alert(abbr);
+alert(com);
+alert(ukr);
+alert(number1);
+alert(number10);
+alert(minus);
+alert(abc);
+alert(pi);
+alert(cdi);
+alert(adult);
+alert(buleT);
+alert(buleF);*/
+
+//3.Promt
+
+/*
+const myName = prompt('Вітаю Леонід');
+const myFatherName = prompt('Вітаю Cтепанович');
+let myAge = prompt('Тобі 36 років');
+
+console.log(myName);
+console.log(myFatherName);
+console.log(myAge);
+
+//4.Concatenate
+
+let fullInfo = myName + ' ' + myFatherName + ' ' + myAge;
+console.log(fullInfo);
+
+*/
+//5.Promt num 3
+
+/*let num1 = + prompt('one');
+let num2 = + prompt('two');
+let num3 = + prompt('three');
+
+console.log(num1);
+console.log(num2);
+console.log(num3);*/
+
+
+//6 ParseInt
+
+/*let numOne = parseInt (prompt ('1'));
+let numTwo = parseInt(prompt('2'));
+let numThree = parseInt(prompt('3'));
+let numFour = parseInt(prompt('4'));
+
+ console.log(numOne);
+console.log(numTwo);
+console.log(numThree);
+console.log(numFour);
+console.log(numOne + numTwo + numThree + numFour);*/
+
+
+//7 Parsefloat
+
+//let numOne = parseFloat (prompt ('one'));
+//let numTwo = parseFloat (prompt ('two'));
+//let numThree = parseFloat (prompt ('three'));
+//let result = (numOne + numTwo + numThree);
+
+ //console.log(numOne,numTwo,numThree);
+ //console.log(result);
+
+
+//  8 mathRound
+
+/*let numOne = Math.round (prompt ('onePoint'));
+let numTwo = Math.round (prompt ('twoPoint'));
+let numThree = Math.round (prompt ('threePoint'));
+
+console.log(numOne, numTwo, numThree);
+console.log(numOne + numTwo + numThree);*/
+
+// 9.Stepin
+
+/*let numer = prompt('chuslo');
+let exponenta = prompt('stepin');
+console.log(Math.pow(numer,exponenta));*/
+
+//10. Typeoff
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// let d = undefined;
+// console.log(typeof d);
+
+
+// 11.Operatory
+
+/*console.log(5<6);
+console.log(5>6);
+console.log(5===6);
+console.log(5==6);
+console.log(10===10);
+console.log(10==10);
+console.log(10<10);
+console.log(10>10);
+console.log(10!=10);
+console.log(123 ==='123');
+console.log(123 =='123');*/
+
+
+// Робота в ауд-рії.
+//  1
+/*let str = 'Привіт';
+let num = 123;
+let flag = true;
+let txt = 'true';
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);*/
+
+
+// 2.
+/*let a1 = 50-20+4;
+let a2 = 6+6*1;
+let a3 = 6*6+30;
+let a4 = (15+30)*2;
+let a5 = 9*9+6;
+console.log(a1,a2,a3,a4,a5);*/
+
+
+// 3.
+ /*let a6 = 5 % 3;
+let a7 = 3 % 5;
+let a8 = 5 +'3';
+let a9 = '5'-3;
+let a10= 75 + 'кг';
+console.log(a6,a7,a8,a9,a10);*/
+
+// 4.
+ /*let height = 23;
+let width = 10;
+let s = height * width + 'cм';
+console.log(s);*/
+
+
+// 5.
+ /*let heightC = 10;
+let dC = 4;
+let pi = 3.14;
+let rC = dC/2;
+let v = pi * Math.pow(rC,2) * heightC;
+console.log(v);*/
+
+// 6.
+/*let n = 3;
+let m = 4;
+let k = Math.sqrt(Math.pow(m,2)+Math.pow(n,2));
+console.log(k);*/
+
+// 7.
+/*let str ='Hello world';
+console.log(str);
+document.write(str);
+alert(str);*/
+
+
+// 8.
+/*const name = 'Шевцов Леонід Степанович';
+let age = 36;
+let hobbi = 'fishing';
+alert(name + '\n'+ age + '\n' + hobbi);*/
+
+//9.
+/*let str1= 'хто';
+let str2= 'ти';
+let str3= 'такий';
+let concatination = str1 +' ' + str2 + ' ' + str3;
+document.write(concatination);*/
+
+//11.
+/*console.log(parseInt('3.14'),parseInt('-7.875'),parseInt('435'),parseInt('Вася') );*/
+
+
+//12.
+/*let str = prompt('Enter something','ho-ho');
+console.log(str);*/
+
+// 13.
+
+/*let num1 = +prompt(5)
+let num2 = +prompt(6)
+alert(num1+num2);*/
+
+// 14.
+/*let userName = prompt('name');
+let surName = prompt('surname');
+let userAge = prompt('age');
+alert('Доброго вечора'+ ' ' + userName + ' ' + surName + 'мої вітання що вам 32' + userAge);*/
+
+
+// Додаткове
+
+// 1.
+ /*let a = +prompt('num1');
+let b = +prompt('num2');
+let c = +prompt('num3');
+
+if (a > b && a > c) {
+    if (b > c) {
+        console.log(c, b, a);
+    } else {
+        console.log(b, c, a);
+    }
+}
+ else  if (b > c && b > a) {
+     if (c > a); {
+         console.log(c,a,b);
+     }
+}
+ else if (c > b && c > a) {
+     if (b > a);
+    {
+        console.log(a,b,c);
+    }
+} else {
+     console.log(b,a,c);
+}*/
+
+
+// 2
+/*let color = prompt('enter color');
+if (color === 'green'){
+    console.log('go');
+} else if (color === 'yellow'){
+    console.log('wait');
+} else if (color === 'red') {
+    console.log('stop');
+} else {
+    console.log('do enything');
+}*/
+
+
+// 3
+ /*let colorTrLight = prompt('enter color');
+let isRoadClear = confirm('the car on the road');
+
+if (colorTrLight === 'green' && isRoadClear === true) {
+    console.log('go');
+} else if ('colorTriLight' === 'green' && isRoadClear === false) {
+    console.log('Wait when will be clear');
+} else if ('colorTriLight' === 'yellow' && isRoadClear === true) {
+    console.log('wait');
+} else if ('colorTriLight' === 'yellow' && isRoadClear === false) {
+    console.log('wait');
+}    else if ('colorTriLight' === 'red' && isRoadClear === false) {
+    console.log('stop,early now');
+}  else if ('colorTriLight' === 'red' && isRoadClear === true) {
+    console.log('stop and wait');
+} else {
+    console.log("do enything");
+}*/
+
+
+
+
+
